321b5b171b2e688ceecd80e9c0d8f93007495bf9
SHASHI BHUSHAN VIJAY <shashibhushan27072002@gmail.com>
2026-01-07 12:37:45 +0000

    ingest_docs succefully executed that leads to creation of vector files

--- backend/app/rag/ingest_docs.py
+++ backend/app/rag/ingest_docs.py
@@ -1,22 +1,33 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os, pickle, faiss
 from sentence_transformers import SentenceTransformer
-import faiss, os, pickle
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-EMBEDDING_MODEL = SentenceTransformer("all-MiniLM-L6-v2")
+EMBED = SentenceTransformer("all-MiniLM-L6-v2")
 VECTOR_PATH = "app/rag/vector_store"
 
-def ingest_pdfs(folder="rag_docs"):
+def ingest_docs(folder="app/knowledge_base"):
     texts = []
+
     for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+
         if file.endswith(".pdf"):
-            loader = PyPDFLoader(os.path.join(folder, file))
+            from langchain_community.document_loaders import PyPDFLoader
+            loader = PyPDFLoader(path)
             texts += loader.load()
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    chunks = splitter.split_documents(texts)
+        elif file.endswith(".txt"):
+            with open(path, "r", encoding="utf-8") as f:
+                texts.append(f.read())
 
-    embeddings = EMBEDDING_MODEL.encode([c.page_content for c in chunks])
+    if not texts:
+        print("No documents found.")
+        return
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    chunks = splitter.create_documents(texts)
+
+    embeddings = EMBED.encode([c.page_content for c in chunks])
 
     index = faiss.IndexFlatL2(384)
     index.add(embeddings)
@@ -24,3 +35,8 @@ def ingest_pdfs(folder="rag_docs"):
     os.makedirs(VECTOR_PATH, exist_ok=True)
     faiss.write_index(index, f"{VECTOR_PATH}/faiss.index")
     pickle.dump(chunks, open(f"{VECTOR_PATH}/docs.pkl","wb"))
+
+    print("RAG knowledge base built successfully.")
+
+if __name__ == "__main__":
+    ingest_docs()
